4ea15de896e1d0a7f244b8fda6b1ed9bb1562f2c
Hannes Gebker <hannes.gebker@d-velop.de>
2025-12-04 21:12:24 +0100

    Update ubuntu configs

---
+++ ubuntu/.config/nvim/lua/config/options.lua
@@ -0,0 +1,21 @@
+-- Options are automatically loaded before lazy.nvim startup
+-- Default options that are always set: https://github.com/LazyVim/LazyVim/blob/main/lua/lazyvim/config/options.lua
+-- Add any additional options here
+--
+--vim.g.node_host_prog = "~/.nvm/versions/node/v24.9.0/bin/node"
+vim.g.loaded_python3_provider = 0
+vim.g.loaded_ruby_provider = 0
+vim.g.loaded_perl_provider = 0
+
+vim.g.clipboard = {
+  name = "WslClipboard",
+  copy = {
+    ["+"] = "clip.exe",
+    ["*"] = "clip.exe",
+  },
+  paste = {
+    ["+"] = 'powershell.exe -c [Console]::Out.Write($(Get-Clipboard -Raw).tostring().replace("`r", ""))',
+    ["*"] = 'powershell.exe -c [Console]::Out.Write($(Get-Clipboard -Raw).tostring().replace("`r", ""))',
+  },
+  cache_enabled = 0,
+}
---
+++ ubuntu/.config/nvim/lua/plugins/neo-tree.lua
@@ -0,0 +1,23 @@
+return {
+  {
+    "ThePrimeagen/vim-be-good",
+    cmd = "VimBeGood",
+  },
+  {
+    "folke/snacks.nvim",
+    opts = {
+      picker = {
+        hidden = true,
+        ignored = true,
+      },
+      explorer = {
+        files = {
+          hidden = true,
+        },
+      },
+      sources = {
+        files = { hidden = true },
+      },
+    },
+  },
+}
